62df45794f4b6606d1f8fd26981f81fc617899e9
de-shyt <de.shutina@gmail.com>
2025-04-30 00:39:29 +0200

    PersonDatabase

---
+++ src/main/java/com/example/springbootproject/services/PersonDatabase.java
@@ -0,0 +1,53 @@
+package com.example.springbootproject.services;
+
+import com.example.springbootproject.entities.Animal;
+import com.example.springbootproject.entities.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class PersonDatabase {
+    private int nextId = 0;
+    private final Map<Integer, Person> database;
+
+    public PersonDatabase() {
+        this.database = new HashMap<>();
+    }
+
+    public Map<Integer, Person> getDatabase() {
+        return database;
+    }
+
+    public Person add(String name, String gender, int age) {
+        int id = nextId++;
+        Person person = new Person(id, name, gender, age);
+        database.put(id, person);
+        return person;
+    }
+
+    public Person add(String name, String gender, int age, Animal animal) {
+        Person person = this.add(name, gender, age);
+        person.setAnimal(animal);
+        animal.setOwner(person);
+        return person;
+    }
+
+    public Person getById(Integer id) {
+        return database.get(id);
+    }
+
+    public List<Person> getByName(String name) {
+        List<Person> result = new ArrayList<>();
+        for (var person : database.values())
+            if (person.getName().equals(name)) result.add(person);
+        return result;
+    }
+
+    public Person remove(Integer id) {
+        return database.remove(id);
+    }
+}

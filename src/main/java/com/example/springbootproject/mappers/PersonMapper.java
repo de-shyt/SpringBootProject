11969d053a27a8c7f3c66cec09b6b39b2f6dcf3d
de-shyt <de.shutina@gmail.com>
2025-05-08 01:29:05 +0200

    change PersonMapper

--- src/main/java/com/example/springbootproject/mappers/PersonMapper.java
+++ src/main/java/com/example/springbootproject/mappers/PersonMapper.java
@@ -3,6 +3,8 @@ package com.example.springbootproject.mappers;
 import com.example.springbootproject.dto.PersonDTO;
 import com.example.springbootproject.entities.Person;
 
+import java.util.List;
+
 public class PersonMapper {
     public static PersonDTO toPersonDTO(Person person) {
         return new PersonDTO(
@@ -11,6 +13,10 @@ public class PersonMapper {
                 person.getGender(),
                 person.getAge()
         );
+    }
+
+    public static List<PersonDTO> toPersonDTO(List<Person> personList) {
+        return personList.stream().map(PersonMapper::toPersonDTO).toList();
     }
 
     public static Person toPerson(PersonDTO personDTO) {
